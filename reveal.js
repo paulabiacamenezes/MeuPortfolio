5d52846acbaac00279fe28cb63eade36d62a568d
rkavebya <heybbyanca@gmail.com>
2023-11-25 15:43:32 -0300

    Atualização com efeito scroll

---
+++ reveal.js
@@ -0,0 +1,113 @@
+window.revelar = ScrollReveal({reset:true}) //window chama todo o documento do site, com o uso do reset é  usado para resetar o documento do site
+
+//TOPO DO SITE
+revelar.reveal('.efeito-txt-topo',{
+    //duração do efeito
+    duration: 3000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo',{
+    //duração do efeito
+    duration: 3000,
+    distance: '90px',
+    delay: 500
+})
+
+//TITULOS DAS SESSÕES
+revelar.reveal('.titulo',{
+    //duração do efeito
+    duration: 2000,
+    distance: '90px'
+})
+
+//ESPECIALIDADES
+revelar.reveal('.efeito-especialidades1',{
+    //duração do efeito
+    duration: 1000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-especialidades2',{
+    //duração do efeito
+    duration: 1000,
+    distance: '90px',
+    delay: 1000
+})
+revelar.reveal('.efeito-especialidades3',{
+    //duração do efeito
+    duration: 1000,
+    distance: '90px',
+    delay: 1500
+})
+
+//SESCTION SOBRE
+revelar.reveal('.efeito-img-sobre',{
+    //duração do efeito
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+})
+revelar.reveal('.efeito-txt-sobre',{
+    //duração do efeito
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'rigth'
+})
+
+//PORTFOLIO
+revelar.reveal('.efeito-projeto1',{
+    //duração do efeito
+    duration: 1000,
+    distance: '50px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-projeto2',{
+    //duração do efeito
+    duration: 1000,
+    distance: '50px',
+    delay: 1000
+})
+revelar.reveal('.efeito-projeto3',{
+    //duração do efeito
+    duration: 1000,
+    distance: '50px',
+    delay: 1500
+})
+
+//FORMULÁRIO
+revelar.reveal('.input1',{
+    //duração do efeito
+    duration: 1000,
+    distance: '50px',
+    delay: 500
+})
+
+revelar.reveal('.input2',{
+    //duração do efeito
+    duration: 1000,
+    distance: '50px',
+    delay: 900
+})
+revelar.reveal('.input3',{
+    //duração do efeito
+    duration: 1000,
+    distance: '50px',
+    delay: 1200
+})
+revelar.reveal('.input4',{
+    //duração do efeito
+    duration: 1000,
+    distance: '50px',
+    delay: 1500
+})
+revelar.reveal('.efeito-btn-enviar',{
+    //duração do efeito
+    duration: 1000,
+    distance: '50px',
+    delay: 1900
+})
