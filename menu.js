5e15e6cab57227183021dfa2d08b9b6ae04233c2
Paula Bianca Menezes dos Santos <110268550+paulabiancamenezes@users.noreply.github.com>
2023-12-15 16:41:09 -0300

    Update menu.js
    acrescentando função do modo dark

--- menu.js
+++ menu.js
@@ -1,6 +1,12 @@
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay')
+let trilho = document.getElementById('trilho')
+        let body = document.querySelector('body')
+
+        trilho.addEventListener('click', ()=>{
+           alert("Essa função está em manutenção no momento :)")
+        })
 
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir')
